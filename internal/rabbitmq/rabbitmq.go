e41dea61b1b3860d657a3e952126921ddca2461b
gdguesser <gdguesser@gmail.com>
2021-07-11 00:11:39 -0300

    creating a basic Go template with connection, queues and messaging with RabbitMQ

---
+++ internal/rabbitmq/rabbitmq.go
@@ -0,0 +1,42 @@
+package rabbitmq
+
+import (
+	"fmt"
+	"github.com/streadway/amqp"
+)
+
+type Service interface {
+	Connect() error
+}
+
+type RabbitMQ struct {
+	Conn *amqp.Connection
+	Channel *amqp.Channel
+}
+
+// Connect establishes a connection to our RabbitMQ instance
+// and declares the queue we are going to be using
+func (r *RabbitMQ) Connect() error {
+	fmt.Println("Connecting to RabbitMQ")
+	var err error
+	r.Conn, err = amqp.Dial("amqp://guest:guest@localhost:5672/")
+	if err != nil {
+		return err
+	}
+	fmt.Println("Sucessfully connected to RabbitMQ")
+
+	r.Channel, err = r.Conn.Channel()
+	if err != nil {
+		return err
+	}
+
+
+
+	return nil
+}
+
+
+// NewRabbitMQService returns a pointer to a new RabbitMQ service
+func NewRabbitMQService() *RabbitMQ {
+	return &RabbitMQ{}
+}
---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gdguesser/go-rabbitmq-template/internal/rabbitmq"
+)
+
+type App struct {
+	Rmq *rabbitmq.RabbitMQ
+
+}
+
+func Run() error {
+	fmt.Println("Go RabbitMQ Template")
+
+	rmq := rabbitmq.NewRabbitMQService()
+	app := App {
+		Rmq: rmq,
+	}
+
+	err := app.Rmq.Connect()
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func main() {
+	if err := Run(); err != nil {
+		fmt.Println("Error setting up application")
+		fmt.Println(err)
+	}
+}
